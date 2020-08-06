06b443ea46bb568eb94a436e389b1000c617e7dd
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-06 01:22:50 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,11 +4,20 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+     if BRANDS.include?(brand)
+    else 
+    BRANDS << brand
   end
+end 
 
+  
+  
+  BRANDS= []
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
 
 end
